f2e127ebf903791f86e80c5460c01fa62b472d80
ricardforner.feedback@gmail.com <ricardforner.feedback@gmail.com@8ca5a1a9-f7aa-ab4c-f333-bac3681d2936>
2012-02-18 12:18:30 +0000

    Version 0.1.2 (tagged). Add movie support

---
+++ tags/0.1.2/ext/appmedia/class.appmedia.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Clase AppMedia
+ *
+ * Esta clase facilita la capa de abstraccion a la aplicacion AppMedia
+ *
+ * @author Ricard Forner
+ * @version 0.1.2
+ * @package appmedia
+ */
+
+include_once('class.appmediabase.php');
+
+class AppMedia extends AppMediaBase {
+
+	public function __construct() {
+		$this->dsn = "sqlite:/mnt/cfinterno/usr/www/bbdd/media_series.sdb";
+	}
+	
+	
+} // fin de la classe
+
+?>
